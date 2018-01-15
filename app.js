cba197b1a2e8ed43c2c19b46f9e9c034695e8804
robcrock <rob@robcrock.com>
2018-01-15 14:16:20 +1100

    Add evenlisteners to remove tasks.

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove a task from the list
+  taskList.addEventListener('click', removeTask);
+  // Remove all tasks
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -40,4 +46,37 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault()
+}
+
+// Remove a task from the list
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear all tasks
+function clearTasks(e) {
+
+  while ( taskList.firstChild ) {
+    taskList.removeChild( taskList.firstChild );
+  }
+
+}
+
+// Filter tasks
+function filterTasks(e) {
+
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll( '.collection-item' ).forEach( function(task) {
+    const item = task.firstChild.textContent;
+    if ( item.toLowerCase().indexOf(text) != -1 ) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  } );
 }
